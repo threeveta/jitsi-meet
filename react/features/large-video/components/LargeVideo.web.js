fc833398c18d8cbba6824a61f8897d2ce49ce1da
Ilko Kacharov <kachar136@gmail.com>
2021-02-01 13:34:50 +0200

    Move full screen handler up on the large web video

--- react/features/large-video/components/LargeVideo.web.js
+++ react/features/large-video/components/LargeVideo.web.js
@@ -96,6 +96,7 @@ class LargeVideo extends Component<Props> {
             <div
                 className = { className }
                 id = 'largeVideoContainer'
+                onDoubleClick = { this._onDoubleClick }
                 style = { style }>
                 <Subject />
                 <InviteMore />
@@ -127,8 +128,7 @@ class LargeVideo extends Component<Props> {
                       * largeVideoWrapper in order to hide/show them.
                       */}
                     <div
-                        id = 'largeVideoWrapper'
-                        onDoubleClick = { this._onDoubleClick }>
+                        id = 'largeVideoWrapper'>
                         <video
                             autoPlay = { !this.props._noAutoPlayVideo }
                             id = 'largeVideo'
